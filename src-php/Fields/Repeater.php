44c346d6eda3f1c73a46fc37ea34c68cbb839216
Marco Mark <marco@silvanite.com>
2018-10-12 12:52:07 +0100

    Repeater Inception

--- src-php/Fields/Repeater.php
+++ src-php/Fields/Repeater.php
@@ -8,11 +8,13 @@ use Illuminate\Http\Request;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Fields\Number;
 use Laravel\Nova\Fields\MorphTo;
+use Laravel\Nova\Fields\MorphMany;
 use Naxon\NovaFieldSortable\Sortable;
 use Laravel\Nova\Http\Requests\NovaRequest;
 use Dewsign\NovaRepeaterBlocks\Fields\Polymorphic;
 use Naxon\NovaFieldSortable\Concerns\SortsIndexEntries;
 use MichielKempen\NovaPolymorphicField\HasPolymorphicFields;
+use Dewsign\NovaRepeaterBlocks\Repeaters\Common\Blocks\TextBlock;
 
 class Repeater extends Resource
 {
@@ -20,6 +22,8 @@ class Repeater extends Resource
     use HasPolymorphicFields;
 
     public static $defaultSortField = 'repeater_block_order';
+
+    public static $morphTo = [];
 
     /**
      * The model the resource corresponds to.
@@ -69,7 +73,90 @@ class Repeater extends Resource
             Text::make('Name'),
             Polymorphic::make('Type')->types($request, $this->types($request))->hideTypeWhenUpdating(),
             Sortable::make('Order', 'id'),
+            $this->morphRepeaters($request),
         ];
+    }
+
+    /**
+     * What type of repeater blocks should be made available to the resource.
+     *
+     * @param Request $request
+     * @return array
+     */
+    public function types(Request $request)
+    {
+        if (!$resourceId = static::getResourceId($request)) {
+            return [];
+        }
+
+        $type = $this->model()->whereId($resourceId)->first()->type;
+
+        if (method_exists($type, 'types')) {
+            return $type::types();
+        };
+
+        if (method_exists($type, 'sourceTypes')) {
+            return $type::sourceTypes();
+        };
+
+        return [];
+    }
+
+    /**
+     * Allow repeaters on this resource if available
+     *
+     * @param Request $request
+     * @return mixed
+     */
+    public function morphRepeaters(Request $request)
+    {
+        if (!$this->repeaterHasSubTypes($request)) {
+            return $this->merge([]);
+        }
+
+        return $this->merge([
+            MorphMany::make(__('Repeaters'), 'repeaters', Repeater::class),
+        ]);
+    }
+
+    /**
+     * Determine if the current repeater has sub-repeater types defined
+     *
+     * @param Request $request
+     * @return boolean
+     */
+    protected function repeaterHasSubTypes(Request $request)
+    {
+        if (!$resourceId = $this->getResourceId($request)) {
+            return false;
+        }
+
+        return method_exists($this->model()->whereId($resourceId)->first()->type, 'types');
+    }
+
+    /**
+     * Get the resource ID of the current repeater item
+     *
+     * @param Request $request
+     * @return mixed
+     */
+    protected function getResourceId(Request $request)
+    {
+        if ($resourceId = $request->get('viaResourceId')) {
+            return $resourceId;
+        };
+
+        parse_str(parse_url($request->server->get('HTTP_REFERER'), PHP_URL_QUERY), $params);
+
+        if ($resourceId = array_get($params, 'viaResourceId')) {
+            return $resourceId;
+        };
+
+        if ($resourceId = $request->route('resourceId')) {
+            return $resourceId;
+        };
+
+        return null;
     }
 
     /**
