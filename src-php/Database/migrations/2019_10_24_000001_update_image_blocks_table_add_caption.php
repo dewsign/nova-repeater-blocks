53be213b4cb68b348ae0ecd35a710541676670ab
Daniel Crewdson <daniel.crewdson@dewsign.co.uk>
2019-10-24 16:56:51 +0100

    fix migration title

--- src-php/Database/migrations/2019_10_24_000001_update_image_blocks_table_add_caption.php
+++ src-php/Database/migrations/2019_10_24_000001_update_image_blocks_table_add_caption.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateImageBlocksTable extends Migration
+class UpdateImageBlocksTableAddCaption extends Migration
 {
     /**
      * Run the migrations.
